ef5f6af575161a2a3d5a9298096765ecdb077009
rikezero <rikezero@gmail.com>
2020-09-24 14:12:25 -0300

    Main Screen

---
+++ app/src/main/java/com/example/blendit_test/view/ActMain.kt
@@ -0,0 +1,20 @@
+package com.example.blendit_test.view
+
+import com.example.blendit_test.base.ActBind
+import com.example.blendit_test.custom.activity
+import com.example.blendit_test.custom.onClick
+import com.example.blendit_test.custom.onClickStart
+import com.example.blendit_test.custom.viewBind
+import com.example.blendit_test.databinding.ActMainBinding
+
+
+class ActMain : ActBind<ActMainBinding>(){
+    override val binding: ActMainBinding by viewBind()
+
+    override fun ActMainBinding.onBinding() {
+        regBtn.onClickStart(ActRegister::class)
+        listBtn.onClickStart(ActListing::class)
+
+    }
+
+}
